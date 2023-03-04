6553c8e1013333fc3c2a7cec0858ac1928536558
NK <oscarfu0721@gmail.com>
2023-03-04 16:22:07 +0000

    intellij going insane

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-@file:Suppress("UnstableApiUsage", "UnstableApiUsage", "UnstableApiUsage", "UnstableApiUsage")
+@file:Suppress("UnstableApiUsage")
 
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import gg.essential.gradle.util.noServerRunConfigs
