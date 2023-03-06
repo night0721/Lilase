1d17063838094c0ddee6858cdf5118894067854f
NK <oscarfu0721@gmail.com>
2023-03-06 21:33:16 +0000

    using back .kts gradle as why not

--- src/main/java/me/night0721/lilase/gui/ImageRenderer.java
+++ src/main/java/me/night0721/lilase/gui/ImageRenderer.java
@@ -1,22 +1,22 @@
 package me.night0721.lilase.gui;
 
 import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.opengl.GL11;
 
 import static me.night0721.lilase.Lilase.mc;
 
 public class ImageRenderer {
     public void draw() {
-        ScaledResolution scaledResolution = new ScaledResolution(mc);
-        GlStateManager.pushMatrix();
-        mc.getTextureManager().bindTexture(new ResourceLocation("lilase:images/crabby.png"));
 //        GlStateManager.color(CurrentColor.getFloatValue(0, 0), CurrentColor.getFloatValue(0, 1), CurrentColor.getFloatValue(0, 2));
-        Gui.drawModalRectWithCustomSizedTexture(scaledResolution.getScaledWidth() / 2 + 64, scaledResolution.getScaledHeight() / 2 + 64, 0, 0, 128, 32, 128, 32);
-        GlStateManager.popMatrix();
+        GL11.glPushMatrix();
+        mc.getTextureManager().bindTexture(new ResourceLocation("lilase:lilase.png"));
+
+//        mc.getTextureManager().bindTexture(new ResourceLocation("lilase:textures/images/crabby.png"));
+        Gui.drawModalRectWithCustomSizedTexture(5, 5, 0, 0, 100, 100, 100, 100);
+        GL11.glPopMatrix();
     }
 
     @SubscribeEvent
