0c66d91239246a0ef1a4e66d847ac112f6c6c33e
NK <oscarfu0721@gmail.com>
2023-03-08 10:53:18 +0000

    render crabby

--- src/main/java/me/night0721/lilase/gui/ImageRenderer.java
+++ src/main/java/me/night0721/lilase/gui/ImageRenderer.java
@@ -4,19 +4,13 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import org.lwjgl.opengl.GL11;
 
 import static me.night0721.lilase.Lilase.mc;
 
 public class ImageRenderer {
     public void draw() {
-//        GlStateManager.color(CurrentColor.getFloatValue(0, 0), CurrentColor.getFloatValue(0, 1), CurrentColor.getFloatValue(0, 2));
-        GL11.glPushMatrix();
-        mc.getTextureManager().bindTexture(new ResourceLocation("lilase:lilase.png"));
-
-//        mc.getTextureManager().bindTexture(new ResourceLocation("lilase:textures/images/crabby.png"));
-        Gui.drawModalRectWithCustomSizedTexture(5, 5, 0, 0, 100, 100, 100, 100);
-        GL11.glPopMatrix();
+        mc.getTextureManager().bindTexture(new ResourceLocation("lilase:textures/images/crab.png"));
+        Gui.drawModalRectWithCustomSizedTexture(0, 50, 0, 0, 100, 100, 100, 100);
     }
 
     @SubscribeEvent
