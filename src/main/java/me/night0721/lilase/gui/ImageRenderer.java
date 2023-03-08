fa79bd94557661c9d1afb04a02213a67697bac83
NK <oscarfu0721@gmail.com>
2023-03-08 12:07:09 +0000

    changing crabby location

--- src/main/java/me/night0721/lilase/gui/ImageRenderer.java
+++ src/main/java/me/night0721/lilase/gui/ImageRenderer.java
@@ -10,7 +10,7 @@ import static me.night0721.lilase.Lilase.mc;
 public class ImageRenderer {
     public void draw() {
         mc.getTextureManager().bindTexture(new ResourceLocation("lilase:textures/images/crab.png"));
-        Gui.drawModalRectWithCustomSizedTexture(0, 50, 0, 0, 100, 100, 100, 100);
+        Gui.drawModalRectWithCustomSizedTexture(100, 0, 0, 0, 100, 100, 100, 100);
     }
 
     @SubscribeEvent
