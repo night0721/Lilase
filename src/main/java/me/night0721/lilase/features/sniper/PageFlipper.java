164841b09b7af5cb2ca184a51c939e72e74ddd8e
Night Kaly <77528305+night0721@users.noreply.github.com>
2023-03-09 15:46:10 +0000

    will continue on page flipper later

--- src/main/java/me/night0721/lilase/features/sniper/PageFlipper.java
+++ src/main/java/me/night0721/lilase/features/sniper/PageFlipper.java
@@ -1,4 +1,10 @@
 package me.night0721.lilase.features.sniper;
 
 public class PageFlipper {
+  public void start() {
+      Utils.checkFooter();
+      if (Utils.cookie) {
+        Utils.sendServerMessage("/ah")
+          
+  }
 }
